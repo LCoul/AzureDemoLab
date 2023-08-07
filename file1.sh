222bb015a0f2917fbd9c42ec79c0fe36ff3808e8
Lessi Coulibaly <54340026+lcoul@users.noreply.github.com>
2023-08-07 08:53:39 -0700

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -1,1 +1,16 @@
-https://github.com/lcoul/Linux/blob/main/file1.sh
+#!/bin/bash
+	
+echo "********* Add sudo user **********"
+sudo adduser bob
+sudo usermod -aG sudo bob
+id bob
+su - bob
+
+echo "********* Create a directory: files **********"
+mkdir files
+
+echo "********* Install unzip **********"
+sudo apt install unzip
+
+echo "********* Download AzureArc.ps1 file **********"
+wget "https://raw.githubusercontent.com/lcoul/AzureDemoLab/main/Scripts/AzureArc.ps1" -P files/
